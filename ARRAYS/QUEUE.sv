825cef310abc440449578f6c1defc0d4ec9ace59
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2025-12-21 20:16:03 +0530

    Add queue module with push and pop operations

--- ARRAYS/QUEUE.sv
+++ ARRAYS/QUEUE.sv
@@ -24,3 +24,30 @@ $display("Types of seeds=%0d names=%p",seeds.size(),seeds.pop_back());
 
 end
 endmodule 
+
+//QUEUE 1
+
+
+module que;
+int que[$];
+int i;
+initial begin
+$display("----------------------------------------");
+$display("                  Q5(B)                 ");
+$display("----------------------------------------");
+for(i=0;i<=5;i++)begin
+que.push_front(i**2);
+que.push_back(2**i);
+end
+$display("%0p",que);
+$display("---------------------------------");
+repeat(2)begin
+$display("pop front :%0d",que.pop_front());
+$display("pop back :%0d",que.pop_back());
+end
+$display("---------------------------------");
+que.delete();
+$display("The size of the queue is : %0d",que.size());
+
+end
+endmodule
