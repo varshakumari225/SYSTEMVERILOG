6fe7bcd6303d04a644f249eec3920c008e09e8a5
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-01-20 20:13:33 +0530

    Create ENVIRONMENT.sv

---
+++ SV_TB/MEMORY/ENVIRONMENT.sv
@@ -0,0 +1,31 @@
+class env;
+
+gen g;
+driver bfm;
+monitor mon;
+coverage cov;
+check c;
+
+function new();
+g=new();
+bfm=new();
+mon=new();
+cov=new();
+c=new();
+endfunction
+
+task run();
+$display("");
+$display("///////////ENVIRONMENT////////////////");
+$display("");
+fork
+g.run();
+bfm.run();
+mon.run();
+cov.run();
+c.run();
+join_any
+
+endtask
+
+endclass
