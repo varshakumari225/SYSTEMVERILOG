df291bb12139ce4b269d77c290ac9014d4be13b4
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-02-13 10:45:14 +0530

    Update GENERATOR.sv

--- SV_TB/MEMORY/GENERATOR.sv
+++ SV_TB/MEMORY/GENERATOR.sv
@@ -5,20 +5,30 @@ virtual intf g_intf;
 task run();
 $display("////////////////GENERATOR//////////////////////");
 g_intf=cfg::vintf;
+
 wait(g_intf.rst==1);
 @(g_intf.cb);
-
-for(int i=0;i<=5;i++)begin
+for(int i=0;i<=255;i++)begin
 tx=new();
-assert(tx.randomize() with {wr_rd==1;});
+/*tx.randomize()with {wr_rd==1;};
+$display("%0d write=%0d",tx.addr,tx.wr_rd);*/
+assert(tx.randomize() with {wr_rd==1;addr==i;wr_data==i;});
+//$display("[%0t] 1[GEN]",$time);
+/*if(i==255)
+$display("i=[%0d]",i);
+*/
 cfg::gen2drv.put(tx);
 end
 
-for(int j=0;j<=5;j++)begin
+for(int i=0;i<=255;i++)begin
 tx=new();
-
+//tx.randomize() with {wr_rd==0;};
 assert(tx.randomize() with {wr_rd==0;});
-g_intf.wr_rd=0;
+/*
+if(i==255)
+$display("i=[%0d]",i);
+*/
+//$display("[%0t] 1[GEN]",$time);
 cfg::gen2drv.put(tx);
 end
 
