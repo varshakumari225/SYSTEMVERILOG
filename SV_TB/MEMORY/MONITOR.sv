ff5f67ccb000656b0d16d83aeb46e5d6eedf96f4
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-02-14 11:48:07 +0530

    Update monitor task for improved transaction handling
    Refactor monitor task to handle read operations correctly.

--- SV_TB/MEMORY/MONITOR.sv
+++ SV_TB/MEMORY/MONITOR.sv
@@ -1,3 +1,4 @@
+
 class monitor;
 transaction tx;
 virtual intf m_intf;
@@ -6,17 +7,22 @@ task run();
 $display("//////////MONITOR//////////");
 m_intf=cfg::vintf;
 wait(m_intf.rst==1);
-forever begin
-tx=new();
-@(m_intf.cb);
-tx.addr=m_intf.cb.addr;
-tx.wr_rd=m_intf.cb.wr_rd;
-tx.wr_data=m_intf.cb.wr_data;
-tx.rd_data=m_intf.cb.rd_data;
-cfg::mon2cov.put(tx);
-cfg::mon2che.put(tx);
-end
+	forever begin
+	@(m_intf.cb);
+	tx=new();
+	tx.addr=m_intf.cb.addr;
+	tx.wr_rd=m_intf.cb.wr_rd;
+	tx.wr_data=m_intf.cb.wr_data;
 
+	if(m_intf.cb.wr_rd==0)begin
+	@(m_intf.cb);
+	@(m_intf.cb);
+	tx.rd_data=m_intf.cb.rd_data;
+	end
+	cfg::mon2cov.put(tx);
+	cfg::mon2che.put(tx);
+	
+	end
 endtask
 
 endclass
