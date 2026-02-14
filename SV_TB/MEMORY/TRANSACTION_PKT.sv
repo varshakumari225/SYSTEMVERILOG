b544fe9656bc6d1e012fec90d9b2658c5683b535
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-02-14 11:51:26 +0530

    Modify transaction class for address and data constraints
    Updated constraints for write and read data, and changed addr to randc.

--- SV_TB/MEMORY/TRANSACTION_PKT.sv
+++ SV_TB/MEMORY/TRANSACTION_PKT.sv
@@ -1,7 +1,11 @@
 class transaction;
-rand bit [7:0]addr;
+randc bit [7:0]addr;
 rand bit [7:0]wr_data;
 rand bit wr_rd;
-     bit [7:0]rd_data;
-constraint c_addr{unique {addr};}
+     bit[7:0]rd_data;
+constraint c_wr_data{wr_data inside{[0:255]};}
+constraint c_rd_data{rd_data inside{[0:255]};}
+
+constraint c_wr_rd{wr_rd inside {0,1};}
 endclass
+
