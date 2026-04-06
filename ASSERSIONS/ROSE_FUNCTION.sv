27bddf724c3c637c1001d2887ef3205f2f558c7c
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-04-06 14:10:29 +0530

    Create ROSE_FUNCTION.sv

---
+++ ASSERSIONS/ROSE_FUNCTION.sv
@@ -0,0 +1,32 @@
+module rose_function;
+reg clk,a;
+
+initial 
+begin
+clk=0;a=0;
+end
+
+always #5 clk=~clk;
+
+initial begin
+$dumpfile("dump.vcd");
+$dumpvars;
+#15;a=1;
+#10;a=0;
+#10;a=1;
+#20;a=0;
+#10;a=1;
+#10;
+$finish;
+end
+sequence seq1;
+@(posedge clk)$rose(a);
+endsequence
+property p;
+seq1;
+endproperty
+assert property (p)
+$display("[%0t] Assersion passed",$time);
+else
+$error("[%0t] Assersion failed",$time);
+endmodule
