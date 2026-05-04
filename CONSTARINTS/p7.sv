831a9f0b517b04d83de8ddb9d8c62598a9504d80
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-04 09:44:27 +0530

    Create p7.sv

---
+++ CONSTARINTS/p7.sv
@@ -0,0 +1,26 @@
+class const5;
+rand int a[];
+
+constraint s_a{a.size==10;}
+constraint v_a{foreach(a[i])
+		if(i%2==0)
+			a[i]==0;
+		else
+			a[i]==(i/2)+1;
+		}
+
+function void post_randomize();
+	$display("ARRAY VALUES : %0p",a);
+endfunction
+
+endclass
+
+module top;
+const5 c_ex5;
+
+initial begin
+c_ex5=new();
+assert(c_ex5.randomize());
+end
+
+endmodule
