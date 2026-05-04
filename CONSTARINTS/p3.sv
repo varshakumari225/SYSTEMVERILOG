20fede9a1cf12bb19441069a40f0579bbe5c7124
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-04 09:29:29 +0530

    Create p3.sv

---
+++ CONSTARINTS/p3.sv
@@ -0,0 +1,23 @@
+class c3;
+rand int a[];
+constraint s_a{a.size==10;}
+constraint v_a{foreach(a[i])
+ 		if(i%2==0)
+			a[i]==(5*i)+5;
+		else
+			a[i]==-5*(i+1);
+		}
+function void post_randomize();
+	$display("ARRAY VALUES : %0p",a);
+endfunction
+
+endclass
+
+module top;
+c3 c3_ex;
+initial begin 
+c3_ex=new();
+assert(c3_ex.randomize());
+end
+
+endmodule
