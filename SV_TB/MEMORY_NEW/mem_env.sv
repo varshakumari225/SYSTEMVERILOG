40fd9b1cb9331feffbe77d8bae2a915d9f8e7817
G. VARSHA KUMARI <gokanavarsha@gmail.com>
2026-05-21 10:56:40 +0530

    Create mem_env.sv

---
+++ SV_TB/MEMORY_NEW/mem_env.sv
@@ -0,0 +1,16 @@
+class env;
+
+driver bfm=new();
+gen g=new();
+
+task run();
+$display("");
+$display("///////////EVNIRONMENT////////////////");
+$display("");
+g.run();
+bfm.run();
+
+
+endtask
+
+endclass
